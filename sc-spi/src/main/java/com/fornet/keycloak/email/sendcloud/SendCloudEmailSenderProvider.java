1aaf3e0cb06681bd53949447af85ee45dd2a1449
timzaak <zsy.evan@gmail.com>
2023-10-01 22:26:40 +0800

    bak it

---
+++ sc-spi/src/main/java/com/fornet/keycloak/email/sendcloud/SendCloudEmailSenderProvider.java
@@ -0,0 +1,4 @@
+package com.fornet.keycloak.email.sendcloud;
+
+public class SendCloudEmailSenderProvider {
+}
---
+++ sc-spi/src/main/java/com/fornet/keycloak/email/sendcloud/SendCloudEmailSenderProviderFactory.java
@@ -0,0 +1,42 @@
+package com.fornet.keycloak.email.sendcloud;
+
+import org.keycloak.Config;
+import org.keycloak.email.EmailSenderProvider;
+import org.keycloak.email.EmailSenderProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ServerInfoAwareProviderFactory;
+
+import java.util.Map;
+
+public class SendCloudEmailSenderProviderFactory implements EmailSenderProviderFactory, ServerInfoAwareProviderFactory {
+    @Override
+    public EmailSenderProvider create(KeycloakSession keycloakSession) {
+        return null;
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public Map<String, String> getOperationalInfo() {
+        return null;
+    }
+}
