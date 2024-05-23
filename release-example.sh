7a8b6ea0d6d2afb9532ab502b6b2d336a850df80
timzaak <zsy.evan@gmail.com>
2024-05-23 14:16:51 +0800

    Update release-example.sh
    add db config

--- release-example.sh
+++ release-example.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# This is an example, you should change admin/password
+# This is an example, you should change admin/password/db config
 docker run -d --name=dev-keycloak -p 8080:8080 -v $(pwd):/opt/keycloak/themes/ \
 -e KEYCLOAK_ADMIN=admin \
 -e KEYCLOAK_ADMIN_PASSWORD=admin \
-quay.io/keycloak/keycloak:latest start --proxy=edge --hostname=sso.fornetcode.com --hostname-admin=localhost
+quay.io/keycloak/keycloak:latest start --proxy=edge --hostname=sso.fornetcode.com --hostname-admin=localhost \
+ --db postgres --db-url-host mypostgres --db-username myuser --db-password change_me
 
